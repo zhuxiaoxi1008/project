1d3c18160d08a60f62d03891194b4406ecf3a086
iecube-zhuxiaoxi <xiaoxi.zhu@iecube.com.cn>
2025-05-23 17:13:14 +0800

    del

--- tutorial/src/plugins/vxeTable.ts
+++
@@ -1,6 +0,0 @@
-import VxeUITable from 'vxe-table'
-import 'vxe-table/lib/style.css'
-
-export const setupVxeTable = (app: any) => {
-  app.use(VxeUITable)
-}
--- tutorial/src/views/login/subTitle.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <p class="text-sm">{{ defaultSettings.title }}，为工程教育而生</p>
-  <p class="text-sm">产品内测中，欢迎体验</p>
-</template>
-
-<script setup>
-import defaultSettings from "@/settings";
-</script>
